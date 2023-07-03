63c80936628ab7f556088f67cee3feb443ac4cbc
M. Arkan Nibrastama <59256068+ArkanNibrastama@users.noreply.github.com>
2023-07-03 09:37:17 +0700

    Create variables.tf

---
+++ infra/resource_area/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "arkan-spotify-analytics-resource"
+}
+
+variable "location" {
+  default = "asia-southeast2"
+}
+
+variable "storage_class" {
+  default = "STANDARD"
+}
+
+variable "bucket_level" {
+  default = true
+}
+
+variable "bucket_access" {
+  default = "enforced"
+}
